87cbf39a66962e60c100bc0f1df24416c8029173
musaddiqaskira <51538990+musaddiqaskira@users.noreply.github.com>
2024-12-13 00:56:22 +0200

    added customization

---
+++ src/admin/app.js
@@ -0,0 +1,32 @@
+import Logo from "../extensions/Kanta.png";
+import favicon from "../../favicon.png";
+
+export default {
+  config: {
+    auth: {
+      logo: Logo,
+    },
+    menu: {
+      logo: Logo,
+    },
+    head: {
+      favicon: favicon,
+    },
+    translations: {
+      en: {
+        "app.components.LeftMenu.navbrand.title": "Kanta Dashboard",
+        "app.components.LeftMenu.navbrand.workplace":
+          "Manage Kanta Administration",
+        "Auth.form.welcome.title": "Welcom to Kanta Exchange Admin",
+        "Auth.form.welcome.subtitle": "Login to your account",
+        "Settings.profile.form.section.experience.interfaceLanguageHelp":
+          "Preference changes will apply only to you.",
+      },
+    },
+    tutorials: false,
+    notifications: { releases: false },
+  },
+  bootstrap(app) {
+    console.log(app);
+  },
+};
